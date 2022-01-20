3d34fabf5053d036f4b4c8eced7742dd1319f20a
Katie Sember <katie.sember@circleci.com>
2022-01-20 12:20:49 -0500

    linting:

--- src/scripts/get-pr-target-branch.sh
+++ src/scripts/get-pr-target-branch.sh
@@ -9,7 +9,7 @@ if [[ -n $CIRCLE_PULL_REQUEST ]]; then
   ## Get pull request number from URL
   PR_NUMBER=$(echo "$CIRCLE_PULL_REQUEST" | sed "s/.*\/pull\///")
   echo "PR_NUMBER: $PR_NUMBER"
-  echo "export GITHUB_PR_NUMBER=$PR_NUMBER" >> $BASH_ENV
+  echo "export GITHUB_PR_NUMBER=$PR_NUMBER" >> "$BASH_ENV"
 
   ## Create GH API to get target branch
   API_GITHUB="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
@@ -18,9 +18,9 @@ if [[ -n $CIRCLE_PULL_REQUEST ]]; then
   echo "PR_REQUEST_URL: $PR_REQUEST_URL"
   PR_RESPONSE=$(curl -H "Authorization: token ${T}" "$PR_REQUEST_URL")
 
-  PR_TARGET_BRANCH=$(echo $PR_RESPONSE | jq -e '.base.ref' | tr -d '"')
+  PR_TARGET_BRANCH=$(echo "$PR_RESPONSE" | jq -e '.base.ref' | tr -d '"')
   echo "PR_TARGET_BRANCH: $PR_TARGET_BRANCH"
-  echo "export GITHUB_PR_TARGET_BRANCH='${PR_TARGET_BRANCH/"'"/}'" >> $BASH_ENV
+  echo "export GITHUB_PR_TARGET_BRANCH='${PR_TARGET_BRANCH/"'"/}'" >> "$BASH_ENV"
 else
   echo "There is no pull request associated with this commit"
 fi
